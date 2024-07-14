c150885a88544d48156a3d176a0c53127c601b94
Nguyễn Văn Huấn <116735061+huanUITk16@users.noreply.github.com>
2024-07-14 14:40:12 +0700

    Update Hook.java (import WebDriver)

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -13,6 +13,7 @@ import io.cucumber.java.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 
